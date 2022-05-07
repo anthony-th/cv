7e3cd7ed78668afe667c6eb6018972d7ffe3b717
Anthony <yefremov.anthony@gmail.com>
2022-05-07 22:53:21 +0300

    refactor: transition edited

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ const i18nObj = {
     'pet-projects': 'Pet-Projects',
     'english': 'English',
     'french': 'French',
-    'experience-text': 'More than 2 years of work as a CNC machine setter. Currently a freelancer.',
+    'experience-text': 'This information is available and presented in the summary.',
     'title-university': 'Moscow Polytechnic University',
     'text-university': 'The rest of the information is available in the resume.',
     'recent-work': 'My Recent Work',
@@ -48,7 +48,14 @@ const i18nObj = {
     'overlay-title-2': 'Project "Portfolio"',
     'overlay-title-3': 'Project "Eco-sounds"',
     'overlay-title-4': 'Project "Random jokes"',
-    'select-all': 'Select and Copy all'
+    'select-all': 'Select and Copy all',
+    'portfolio-text-info': 'This section presents projects from the "JS/FE Pre-School 2022" and "JavaScript/Front-end 2022Q1" courses.',
+    'title-stack': 'My stack',
+    'language-skill': 'Language skill',
+    'overlay-text-1': 'Status: in progress',
+    'overlay-text-2': 'The page is adaptive, with language switch and dark/light modes. This layout is made with PerfectPixel for a perfect match with the layout from Figma',
+    'overlay-text-3': 'This project implements the playback of bird voices by the advised labels and the play/pause button',
+    'overlay-text-4': 'This application displays a random quote from the API when the button is clicked (with sound and animation). The json file is used as the source of the quote. This application has a ru\\eng language switch'
   },
   'ru': {
     'home-anchor': 'Домой',
@@ -73,7 +80,7 @@ const i18nObj = {
     'pet-projects': 'Свои проекты',
     'english': 'Английский',
     'french': 'Французский',
-    'experience-text': 'Более 2 лет работы в качестве наладчика станков с ЧПУ. В настоящее время - фрилансер.',
+    'experience-text': 'Данная информация доступна и представлена в резюме.',
     'title-university': 'Московский политехнический университет',
     'text-university': 'Остальную информацию можно найти в резюме.',
     'recent-work': 'Мои последние работы',
@@ -85,7 +92,14 @@ const i18nObj = {
     'overlay-title-2': 'Проект "Портфолио"',
     'overlay-title-3': 'Проект "Звуки природы"',
     'overlay-title-4': 'Проект "Случайные шутки"',
-    'select-all': 'Выделить и копировать всё'
+    'select-all': 'Выделить и копировать всё',
+    'portfolio-text-info': 'В этом разделе представлены проекты, выполненные в рамках программы курсов "JS/FE Pre-School 2022" и "JavaScript/Front-end 2022Q1".',
+    'title-stack': 'Мой стэк',
+    'language-skill': 'Знания языка',
+    'overlay-text-1': 'Статус: в процессе выполнения',
+    'overlay-text-2': 'Страница адаптивная, с переключением языка и режимами темный/светлый. Этот макет сделан с помощью PerfectPixel для идеального совпадения с макетом из Figma',
+    'overlay-text-3': 'В этом проекте реализовано воспроизведение голосов птиц по предложенным надписям и кнопке воспроизведения/паузы',
+    'overlay-text-4': 'Это приложение отображает случайную цитату из API при нажатии на кнопку (со звуком и анимацией). В качестве источника цитаты используется json-файл. Это приложение имеет переключатель языка ru\\eng'
   }
 }
 
